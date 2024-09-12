7171bd96215c4b931b2f8a7bf936671a92f5dbd8
ariclinis <ariclinis1@gmail.com>
2024-09-12 17:03:57 +0100

    capistrana setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,18 @@
 set :application, "bravos"
 set :repo_url, "git@github.com:okueca/bravos.git"
 set :branch, "main"
-set :deploy_to, "/home/mudex/apps/#{fetch(:application)}"
+set :deploy_to, "/home/#{ENV.fetch('SERVER_USER_ORGANIZATION')}/apps/#{fetch(:application)}"
+set :user, "#{ENV.fetch('SERVER_USER_ORGANIZATION')}"
 
-set :ssh_options,     { forward_agent: true, user: "mudex", keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 
-set :tmp_dir, "/home/mudex/tmp"
+set :tmp_dir, "/home/#{ENV.fetch('SERVER_USER_ORGANIZATION')}/tmp"
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 # setup rbenv.
 set :rbenv_type, :system
 set :rbenv_ruby, '3.0.6'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_path, '/home/mudex/.rbenv'
+set :rbenv_path, "/home/#{ENV.fetch('SERVER_USER_ORGANIZATION')}/.rbenv"
 
 set :passenger_restart_with_touch, false
-
-set :assets_manifests, ['app/assets/config/manifest.js']
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :branch, 'main'
-set :server_address, "#{ENV.fetch('SERVER_USER_ORGANIZATION')}"
+set :server_address, "#{ENV.fetch('SERVER_IP_ORGANIZATION')}"
 server fetch(:server_address), user: "#{ENV.fetch('SERVER_USER_ORGANIZATION')}", roles: %w{app db web}
  
 set :nginx_server_name, fetch(:server_address)
